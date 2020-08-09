6cceb5e2fe49052f4ac78a2de048f35464682039
Matthew Fan <fn.matthew@outlook.com>
2020-08-08 22:47:50 -0500

    Sectioned off some space for the GraphApp Control Panel

---
+++ src/components/GAControlPanel/GAControlPanel.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./GAControlPanel.scss";
+
+
+class GAControlPanel extends React.Component {
+    render() {
+        return(
+            <div className="gacp">
+                <div className="gacp-menu">
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default GAControlPanel;
--- src/components/GraphApp/GraphApp.js
+++ src/components/GraphApp/GraphApp.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 import Space from "./../Space/Space.js";
+import GAControlPanel from "./../GAControlPanel/GAControlPanel.js";
+
+import "./GraphApp.scss";
 
 
 class GraphApp extends React.Component {
     render() {
         return(
-            <div className={this.props.className}>
+            <div className={this.props.className + " graphApp"}>
                 <Space />
+                <GAControlPanel />
             </div>
         );
     }
